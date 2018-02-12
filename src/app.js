a97dcb551d5912d5c6ec71b5866463db8ddb83d2
chrisperk <christopherperkins.512@gmail.com>
2018-02-12 17:45:13 -0600

    working on fetching data from AAC on interval

--- src/app.js
+++ src/app.js
@@ -1,9 +1,12 @@
+require('dotenv').config();
 var express = require('express');
 var app = express();
 var mongoose = require('mongoose');
 var config = require('../config');
+var https = require('https');
 
-var port = process.env.PORT || 3000;
+var port = process.env.PORT || 3005;
+console.log(port);
 
 mongoose.connect(config.getDbConnectionString())
     .then(function () {
@@ -13,6 +16,29 @@ mongoose.connect(config.getDbConnectionString())
         console.log('[mongoose] Error connecting to MongoDB')
     });
 
+const options = {
+    hostname: 'data.austintexas.gov',
+    path: '/resource/fdzn-9yqv.json',
+    data: {
+        limit: 50000
+    }
+}
 
+function getAnimalData() {
+    https.get(options, function (fetched) {
+        console.log(fetched);
+        fetched.setEncoding('utf8');
+        let body = '';
+        fetched.on('data', function (data) {
+            body += data;
+        });
+        fetched.on('end', function () {
+            body = JSON.parse(body);
+            console.log(body);
+        });
+    });
+}
+
+setInterval(getAnimalData, 5000);
 
 app.listen(port);
